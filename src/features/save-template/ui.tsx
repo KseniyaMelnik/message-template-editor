93bcc121f8a27450cc32b4af0c2e798c8bcd2ecc
kseniya <kseniyamelnnik@gmail.com>
2023-07-19 23:58:59 +0300

    feat: add toast

--- src/features/save-template/ui.tsx
+++ src/features/save-template/ui.tsx
@@ -1,14 +1,38 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
+
+import { createPortal } from 'react-dom'
 
 import { EditElementType } from 'entities/edit-elements-list'
 import { Button } from 'shared/ui/button/ui'
+import { Toast } from 'shared/ui/toast'
 
 type SaveTemplateProps = {
   template: EditElementType[]
   callbackSave: (template: EditElementType[]) => void
 }
 export const SaveTemplate: FC<SaveTemplateProps> = ({ template, callbackSave }) => {
-  const onSave = () => {}
+  const onSave = () => {
+    try {
+      callbackSave(template)
+      if (timeoutId) clearTimeout(timeoutId) // очистить таймаут
+      setMessage('Template successfully saved')
+    } catch {
+      setMessage('Something went wrong')
+    } finally {
+      setTimeoutId(
+        setTimeout(() => {
+          setMessage('')
+        }, 3000)
+      )
+    }
+  }
+  const [message, setMessage] = useState('')
+  const [timeoutId, setTimeoutId] = useState<ReturnType<typeof setTimeout> | null>(null)
 
-  return <Button onClick={() => callbackSave(template)}>Save</Button>
+  return (
+    <>
+      <Button onClick={onSave}>Save</Button>
+      {message && createPortal(<Toast text={message} />, document.body)}
+    </>
+  )
 }
--- src/pages/message-editor/index.tsx
+++ src/pages/message-editor/index.tsx
@@ -4,6 +4,7 @@ import s from './styles.module.scss'
 
 import { EditElementType } from 'entities/edit-elements-list'
 import { ToggleShowEditor } from 'features/toggle-show-editor'
+import { Toast } from 'shared/ui/toast'
 import { MessageTemplateEditor } from 'widgets/message-template-editor'
 
 const arrVarNames = localStorage.arrVarNames
@@ -15,7 +16,17 @@ const template = localStorage.template ? JSON.parse(localStorage.template) : nul
 const callbackSave = async (template: EditElementType[]) => {
   const stringTemplate = JSON.stringify(template)
 
-  localStorage.setItem('template', stringTemplate)
+  try {
+    localStorage.setItem('template', stringTemplate)
+
+    return <Toast text={'Success'} />
+    // показываем сообщение об успешном сохранении данных
+    //showSuccessMessage('Данные успешно сохранены в localStorage')
+  } catch (error) {
+    // показываем сообщение об ошибке
+    //showErrorMessage('Не удалось сохранить данные в localStorage')
+    console.error(error)
+  }
 }
 
 export const MessageEditor = () => {
